f3933f306483ea7fb9d528f5e499c4ffa512f136
Erick Maia <erickmaiatmariano@gmail.com>
2023-01-30 20:11:44 -0300

    Comment added 'HostListener also works with custom events'

--- SandBox/src/app/Directives/revealed-message.directive.ts
+++ SandBox/src/app/Directives/revealed-message.directive.ts
@@ -7,6 +7,7 @@ export class RevealedMessageDirective {
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
+  //HostListener also works with custom events
   @HostListener('customEventEmitter') messageWasRevealed(event: Event){
     console.log("helloooo there")
     
