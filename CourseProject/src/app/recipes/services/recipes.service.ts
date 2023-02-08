8c4e40b5c38201d8a13284bd3ec3a2b8d323b9d3
Erick Maia <erickmaiatmariano@gmail.com>
2023-02-08 01:07:20 -0300

    Lesson 121. Adding ingredients to recipes

--- CourseProject/src/app/recipes/services/recipes.service.ts
+++ CourseProject/src/app/recipes/services/recipes.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 import { Recipe } from '../recipe.model';
 
 export class RecipesService {
@@ -6,8 +7,20 @@ export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>(); 
 
   private recipes: Recipe[] = [
-    new Recipe('Toasted bread', 'Toasted bread with some special ingredients', 'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'), 
-    new Recipe('Chinese rice', 'Traditional chinese recipe', 'https://images.pexels.com/photos/343871/pexels-photo-343871.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+    new Recipe(
+      'Toasted bread', 
+      'Toasted bread with some special ingredients', 
+      'https://images.pexels.com/photos/1775043/pexels-photo-1775043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 
+      [new Ingredient('Bread', 1), 
+       new Ingredient('Butter', 1)]), 
+    new Recipe(
+      'Chinese rice', 
+      'Traditional chinese recipe', 
+      'https://images.pexels.com/photos/343871/pexels-photo-343871.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 
+      [new Ingredient('Rice', 1), 
+       new Ingredient('Fish sauce', 1), 
+       new Ingredient('Oyster sauce', 1), 
+       new Ingredient('Soy sauce', 1)])
   ]
 
   constructor() { }
