63232038a5d5a704a9924d56b2cdbde15dfc4746
Erick Maia <erickmaiatmariano@gmail.com>
2023-05-02 23:14:00 -0300

    Lesson 139. Practicing and some common gotchas

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -14,10 +14,11 @@ import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent }, 
-  { path: 'servers', component: ServersComponent}, 
-  { path: 'servers/:id/edit', component: EditServerComponent}, 
   { path: 'users', component: UsersComponent }, 
-  { path: 'users/:id/:name', component: UserComponent }
+  { path: 'users/:id/:name', component: UserComponent },
+  { path: 'servers', component: ServersComponent}, 
+  { path: 'servers/:id', component: ServerComponent}, 
+  { path: 'servers/:id/edit', component: EditServerComponent}, 
 ]
 
 @NgModule({
--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,10 +11,17 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    //We may use Number.parseInt instead of the + sign below
+    let serverId: number = +(this.activatedRoute.snapshot.params['id']);
+    this.server = this.serversService.getServer(serverId);
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']); 
+      }
+    )
   }
 
 }
