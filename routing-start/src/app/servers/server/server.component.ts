1af8d5b162082c60568786b5ae08fbe80f986945
Erick Maia <erickmaiatmariano@gmail.com>
2023-06-09 01:40:06 -0300

    Lesson 152. Resolving dynamic data with the resolve guard

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Data, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -12,19 +12,24 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(
-    private serversService: ServersService, 
+    // private serversService: ServersService, 
     private activatedRoute: ActivatedRoute, 
     private router: Router) { }
 
   ngOnInit() {
-    //We may use Number.parseInt instead of the + sign below
-    let serverId: number = +(this.activatedRoute.snapshot.params['id']);
-    this.server = this.serversService.getServer(serverId);
-    this.activatedRoute.params.subscribe(
-      (params: Params) => {
-        this.server = this.serversService.getServer(+params['id']); 
+    this.activatedRoute.data.subscribe(
+      (data: Data) => {
+        this.server = data['server']
       }
     )
+    //We may use Number.parseInt instead of the + sign below
+    // let serverId: number = +(this.activatedRoute.snapshot.params['id']);
+    // this.server = this.serversService.getServer(serverId);
+    // this.activatedRoute.params.subscribe(
+    //   (params: Params) => {
+    //     this.server = this.serversService.getServer(+params['id']); 
+    //   }
+    // )
   }
 
   onEdit(){
