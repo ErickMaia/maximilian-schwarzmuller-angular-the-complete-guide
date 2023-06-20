98c372dc6009f0a0cd8b59331b493003c14b4ebc
Erick Maia <erickmaiatmariano@gmail.com>
2023-06-20 02:31:09 -0300

    Lesson 174. Building a custom Observable

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,18 +8,35 @@ import { Subscription, interval } from 'rxjs';
 })
 export class HomeComponent implements OnInit, OnDestroy {
 
-  counter: Subscription
+  subscription: Subscription
 
   constructor() { }
 
   ngOnInit() {
-    this.counter = interval(1000).subscribe(
-      count => console.log(count)
+    // this.subscription = interval(1000).subscribe(
+    //   count => console.log(count)
+    // )
+    const customIntervalObservable = new Observable(
+      observer => {
+        let count = 0
+        setInterval(
+          () => {
+            observer.next(count)
+            count++
+          }
+          ,
+          1000
+        )
+      }
+    )
+
+    this.subscription = customIntervalObservable.subscribe(
+      (data) => console.log(data)
     )
   }
 
   ngOnDestroy(): void {
-    this.counter.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
   
