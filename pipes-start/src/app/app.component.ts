2203df8490987d1d172031caffbd5ad6021b1aee
Erick Maia <erickmaiatmariano@gmail.com>
2023-07-24 18:03:31 -0300

    Lesson 249. Pure and impure Pipes (or how to 'fix' the filter Pipe)

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -35,6 +35,18 @@ export class AppComponent {
 
   filteredStatus = ''
 
+  onAddServer(){
+    console.log("adding new server");
+    
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server', 
+      status: 'stable', 
+      started: new Date(15, 1, 2017)
+    }
+    )
+  }
+
   getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
     return {
       'list-group-item-success': server.status === 'stable',
