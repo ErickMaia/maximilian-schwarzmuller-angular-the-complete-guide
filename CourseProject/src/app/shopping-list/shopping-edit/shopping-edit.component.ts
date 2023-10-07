b2959eac0d9e136818cc213c08f05f885381679e
Erick Maia <erickmaiatmariano@gmail.com>
2023-10-07 18:51:22 -0300

    Lesson 223. Updating existing items

--- CourseProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ CourseProject/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -42,7 +42,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     
     const newIngredient = new Ingredient(form.value.name, form.value.amount)
 
-    this.shoppingListService.addIngredient(newIngredient)
+    if(this.editMode){
+      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient)
+    }else{
+      this.shoppingListService.addIngredient(newIngredient)
+    }
   }
 
   ngOnDestroy(): void {
