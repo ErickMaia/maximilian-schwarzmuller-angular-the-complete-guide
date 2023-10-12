4d77b0f23dc6d40c235238b8ba99227cc45ebdfb
Erick Maia <erickmaiatmariano@gmail.com>
2023-10-12 14:45:51 -0300

    Lesson 233. Validating user input

--- CourseProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ CourseProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipesService } from '../services/recipes.service';
 
@@ -43,8 +43,11 @@ export class RecipeEditComponent implements OnInit {
         for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              'name': new FormControl(ingredient.name),
-              'amount': new FormControl(ingredient.amount)
+              'name': new FormControl(ingredient.name, Validators.required),
+              'amount': new FormControl(ingredient.amount, [
+                Validators.required, 
+                Validators.pattern(/^[1-9]+[0-9]*$/)
+              ])
             })
           )
         }
@@ -53,9 +56,9 @@ export class RecipeEditComponent implements OnInit {
 
     this.recipeForm = new FormGroup(
       {
-        'name': new FormControl(recipeName),
-        'imagePath': new FormControl(recipeImagePath),
-        'description': new FormControl(recipeDescription),
+        'name': new FormControl(recipeName, Validators.required),
+        'imagePath': new FormControl(recipeImagePath, Validators.required),
+        'description': new FormControl(recipeDescription, Validators.required),
         'ingredients': recipeIngredients
       }
     );
@@ -70,12 +73,15 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onAddIngredient() {
-    let ingredients = (<FormArray>this.recipeForm.get('ingredients')).controls
+    let ingredients = (<FormArray>this.recipeForm.get('ingredients'))
 
     ingredients.push(
       new FormGroup({
-        'name': new FormControl(),
-        'amount': new FormControl()
+        'name': new FormControl(null, Validators.required),
+        'amount': new FormControl(null, [
+          Validators.required, 
+          Validators.pattern(/^[1-9]+[0-9]*$/)
+        ])
       })
     )
   }
