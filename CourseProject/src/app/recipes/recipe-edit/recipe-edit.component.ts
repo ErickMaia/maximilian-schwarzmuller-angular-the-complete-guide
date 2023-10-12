0d2178625d9bbea1ac9694fbdb54598fc0a9a513
Erick Maia <erickmaiatmariano@gmail.com>
2023-10-12 13:05:51 -0300

    Lesson 232. Adding new ingredient controls

--- CourseProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ CourseProject/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -10,7 +10,7 @@ import { RecipesService } from '../services/recipes.service';
 })
 export class RecipeEditComponent implements OnInit {
 
-  id:number
+  id: number
   editMode: boolean = false
   recipeForm: FormGroup
 
@@ -22,51 +22,62 @@ export class RecipeEditComponent implements OnInit {
     this.activatedRoute.params.subscribe(
       (params: Params) => {
         this.id = +params['id']
-        this.editMode = (params['id'] != null); 
+        this.editMode = (params['id'] != null);
         this.initForm();
       }
     )
   }
 
-  private initForm(){
-    let recipeName = ''; 
-    let recipeImagePath = ''; 
-    let recipeDescription = ''; 
-    let recipeIngredients = new FormArray([]); 
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
-    if(this.editMode){
+    if (this.editMode) {
       const recipe = this.recipesService.getRecipe(this.id)
       recipeName = recipe.name
       recipeImagePath = recipe.imagePath
       recipeDescription = recipe.description
-      if(recipe['ingredients']){
-        for(let ingredient of recipe.ingredients){
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              'name': new FormControl(ingredient.name), 
+              'name': new FormControl(ingredient.name),
               'amount': new FormControl(ingredient.amount)
             })
           )
         }
       }
     }
-    
+
     this.recipeForm = new FormGroup(
       {
-        'name': new FormControl(recipeName), 
+        'name': new FormControl(recipeName),
         'imagePath': new FormControl(recipeImagePath),
-        'description': new FormControl(recipeDescription), 
+        'description': new FormControl(recipeDescription),
         'ingredients': recipeIngredients
       }
-    ); 
+    );
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.recipeForm)
   }
 
-  get controls(){
+  get controls() {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
+  onAddIngredient() {
+    let ingredients = (<FormArray>this.recipeForm.get('ingredients')).controls
+
+    ingredients.push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
   }
 
 }
