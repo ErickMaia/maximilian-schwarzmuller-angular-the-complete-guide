af96fa2368ef5708054bf7b1aa4e51ec17ca78b9
Erick Maia <erickmaiatmariano@gmail.com>
2023-11-29 00:23:52 -0300

    279. Multiple Interceptors

--- http-01-start/src/app/app.module.ts
+++ http-01-start/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AuthInterceptorService } from './services/auth-interceptor.service';
+import { LoggingInterceptorService } from './services/logging-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
+  // The interceptors are executed in the order of declaration in the providers array. 
+  // Their changes on the HtttpRequest object are cumulative. 
   providers: [
     {
       provide: HTTP_INTERCEPTORS, 
       useClass: AuthInterceptorService, 
       multi: true
-    }
+    }, 
+    {
+      provide: HTTP_INTERCEPTORS, 
+      useClass: LoggingInterceptorService, 
+      multi: true
+    },
   ],
   bootstrap: [AppComponent]
 })
--- http-01-start/src/app/services/auth-interceptor.service.ts
+++ http-01-start/src/app/services/auth-interceptor.service.ts
@@ -1,24 +1,12 @@
-import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { tap } from "rxjs/operators";
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler){
-    console.log("Your HTTP request was intercepted. ")
 
     const interceptedRequest = req.clone({
       headers: req.headers.append("Authorization", "Bearer xyz123")
     })
 
     return next.handle(interceptedRequest)
-      .pipe(
-        tap(
-          event => {
-            if(event.type === HttpEventType.Response){
-              console.log("Response arrived. Body data: ")
-              console.log(event.body)
-            }
-          }
-        )
-      ); 
   }
 }
