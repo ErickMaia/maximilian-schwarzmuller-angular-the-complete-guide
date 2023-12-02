1e3404a5283513ffcd54f6b973ee7218a2a83e19
Erick Maia <erickmaiatmariano@gmail.com>
2023-12-01 21:52:48 -0300

    Lesson 285. Storing Recipes

--- CourseProject/src/app/shared/data-storage.service.ts
+++ CourseProject/src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RecipesService } from '../recipes/services/recipes.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,19 @@ import { Injectable } from '@angular/core';
 export class DataStorageService {
 
   constructor(
-    private httpClient: HttpClient
-  ) { }
+    private httpClient: HttpClient, 
+    private recipesService: RecipesService
+  ) {
+
+   }
+
+  storeRecipes(){
+    const recipes = this.recipesService.getRecipes(); 
+
+    this.httpClient.put("http://localhost:5020/api/Recipes", recipes)
+    .subscribe(response => {
+      console.log(response); 
+    }); 
+    ; 
+  }
 }
