1cfa3dc224bbc15d97e19baf625b1bd5e55e00a3
Erick Maia <erickmaiatmariano@gmail.com>
2023-12-17 22:35:14 -0300

    Lesson 298. Sending the sign up request

--- CourseProject/src/app/auth/auth.component.ts
+++ CourseProject/src/app/auth/auth.component.ts
@@ -1,11 +1,17 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth', 
     templateUrl: './auth.component.html'
 })
 export class AuthComponent{
+
+    constructor(private authService: AuthService){
+
+    }
+
     isLoginMode = true; 
 
     onSwitchMode(){
@@ -13,7 +19,29 @@ export class AuthComponent{
     }
 
     onSubmit(authForm: NgForm){
-        console.log(authForm.value)
+        if(authForm.invalid){
+            return; 
+        }
+
+        const email = authForm.value.email; 
+        const password = authForm.value.password; 
+
+
+        if(this.isLoginMode){
+            // TODO add login logic
+        }else{
+
+        this.authService.signUp(email, password)
+            .subscribe(
+                response => {
+                    console.log(response)
+                },
+                error => {
+                    console.log(error)
+                }
+            )
+        }
+
         authForm.reset()
     }
 }
