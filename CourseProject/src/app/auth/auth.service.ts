4fe2a1c52032712f19758223f1f21a0d456dac52
Erick Maia <erickmaiatmariano@gmail.com>
2023-12-28 11:49:21 -0300

    Lesson 303. Creating & Storing the User Data

--- CourseProject/src/app/auth/auth.service.ts
+++ CourseProject/src/app/auth/auth.service.ts
@@ -1,80 +1,111 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { User } from './user.model';
 
-export interface AuthResponseData{
-    kind: string
-    idToken: string
-    email: string
-    refreshToken: string
-    expiresIn: string
-    localId: string
-    registered?: boolean
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthService{
+export class AuthService {
+  user = new Subject<User>();
 
-    constructor(private httpClient: HttpClient){
+  constructor(private httpClient: HttpClient) {}
 
+  // Just an API key for testing purposes on this course. It doesn't store important information and there is no problem for it to be explicit here.
+  apiKey = 'AIzaSyDqCggIG453WcBWmiFPbxucyaRRmdaqwRg';
+
+  signUp(email: string, password: string) {
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+      this.apiKey;
+
+    return this.httpClient
+      .post<AuthResponseData>(url, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) => {
+          this.handleAutentication(
+            responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAutentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+
+    const user = new User(email, userId, token, expirationDate);
+
+    this.user.next(user);
+  }
+
+  login(email: string, password: string) {
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+      this.apiKey;
+
+    return this.httpClient
+      .post<AuthResponseData>(url, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) => {
+          this.handleAutentication(
+            responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMessage = 'An unknown error ocurred.';
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
     }
 
-    // Just an API key for testing purposes on this course. It doesn't store important information and there is no problem for it to be explicit here.
-    apiKey = "AIzaSyDqCggIG453WcBWmiFPbxucyaRRmdaqwRg"
-
-    signUp(email: string, password: string){
-
-        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + this.apiKey
-
-        return this.httpClient.post<AuthResponseData>
-        (
-            url, 
-            {
-                email: email, 
-                password: password, 
-                returnSecureToken: true
-            }
-        ).pipe(catchError(this.handleError))
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage =
+          'The email address is already in use by another account.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+      case 'INVALID_PASSWORD':
+      case 'INVALID_LOGIN_CREDENTIALS':
+        errorMessage =
+          'Invalid login. Please check if the email and password provided are correct.';
+        break;
     }
 
-
-    login(email: string, password: string){
-        let url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + this.apiKey
-
-        return this.httpClient.post<AuthResponseData>(
-            url, 
-            {
-                email: email, 
-                password: password, 
-                returnSecureToken: true
-            }
-        ).pipe(catchError(this.handleError))
-
-    }
-
-    private handleError(errorResponse: HttpErrorResponse){
-        
-        let errorMessage = "An unknown error ocurred."
-
-        if(!errorResponse.error || !errorResponse.error.error){
-            return throwError(errorMessage); 
-        }
-
-        switch(errorResponse.error.error.message){
-            case 'EMAIL_EXISTS': 
-                errorMessage = "The email address is already in use by another account."
-                break;
-            case "EMAIL_NOT_FOUND": 
-            case "INVALID_PASSWORD": 
-            case "INVALID_LOGIN_CREDENTIALS":
-                errorMessage = "Invalid login. Please check if the email and password provided are correct."
-                break;
-        }
-
-        return throwError(errorMessage); 
-    }
-
+    return throwError(errorMessage);
+  }
 }
