eb47d8adcdb46ccf7cb6149a81c4e134bf2553f4
Erick Maia <erickmaiatmariano@gmail.com>
2024-01-07 16:11:44 -0300

    Lesson 308. Adding Auto-Login

--- CourseProject/src/app/auth/auth.service.ts
+++ CourseProject/src/app/auth/auth.service.ts
@@ -62,6 +62,8 @@ export class AuthService {
     const user = new User(email, userId, token, expirationDate);
 
     this.user.next(user);
+
+    localStorage.setItem('userData', JSON.stringify(user))
   }
 
   login(email: string, password: string) {
@@ -89,8 +91,26 @@ export class AuthService {
   }
 
   logout(){
+    localStorage.removeItem('userData')
     this.user.next(null)
     this.router.navigate(['/auth']);
+  }
+
+  autoLogin(){
+    const userData: {
+      email: string, 
+      id: string,
+      _token: string, 
+      _tokenExpirationDate: string
+     } = JSON.parse(localStorage.getItem('userData'))
+
+    if(!userData) return
+
+    const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate))
+
+    if(loadedUser.token){
+      this.user.next(loadedUser); 
+    }
   }
 
   private handleError(errorResponse: HttpErrorResponse) {
