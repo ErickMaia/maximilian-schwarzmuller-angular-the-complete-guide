ba1e3e0c7b737cfe8ba533b59364bbf2dbc3f1ea
Erick Maia <erickmaiatmariano@gmail.com>
2024-01-10 21:31:56 -0300

    Lesson 333. Understanding the core module

--- CourseProject/src/app/app.module.ts
+++ CourseProject/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/services/shopping-list.service';
-import { RecipesService } from './recipes/services/recipes.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -33,15 +27,7 @@ import { SharedModule } from './shared/shared.module';
     RecipesModule, 
     ShoppingListModule, 
     SharedModule, 
-  ],
-  providers: [
-    ShoppingListService,
-    RecipesService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    CoreModule
   ],
   bootstrap: [AppComponent],
 })
