18cba0fb72864648e684303cd38dd08110d34503
Erick Maia <erickmaiatmariano@gmail.com>
2024-01-10 21:48:29 -0300

    Lesson 334. Adding an auth feature module

---
+++ CourseProject/src/app/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { AuthComponent } from "./auth.component";
+import { SharedModule } from "../shared/shared.module";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations: [
+        AuthComponent
+    ], 
+    imports: [
+        SharedModule, 
+        FormsModule,
+        RouterModule.forChild(
+            [{path: 'auth', component: AuthComponent}]
+        )
+    ]
+})
+export class AuthModule{
+
+}
