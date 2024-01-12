2c3ed95b6afd797ca4b002eb93e9ebdc2b97a605
Erick Maia <erickmaiatmariano@gmail.com>
2024-01-11 23:01:22 -0300

    Lesson 337. More lazy loading

--- CourseProject/src/app/auth/auth.module.ts
+++ CourseProject/src/app/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { FormsModule } from "@angular/forms";
         SharedModule, 
         FormsModule,
         RouterModule.forChild(
-            [{path: 'auth', component: AuthComponent}]
+            [{path: '', component: AuthComponent}]
         )
     ]
 })
