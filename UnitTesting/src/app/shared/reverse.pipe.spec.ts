76cdc7199e45d392384d100d5b905ed4f20d5f64
Erick Maia <erickmaiatmariano@gmail.com>
2024-05-14 14:07:43 -0300

    Lesson 470. Isolated vs Non-Isolated Tests

---
+++ UnitTesting/src/app/shared/reverse.pipe.spec.ts
@@ -0,0 +1,10 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReversePipe } from './reverse.pipe';
+
+
+describe('Pipe: ReversePipe', () => {
+  it('should create the app', () => {
+    let reversePipe = new ReversePipe(); 
+    expect(reversePipe.transform('hello')).toEqual('olleh')
+  });
+});
