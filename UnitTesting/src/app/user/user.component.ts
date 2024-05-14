8fddfcf0d94bea56a8ef96d8672f3319a2ebcaff
Erick Maia <erickmaiatmariano@gmail.com>
2024-05-14 13:49:14 -0300

    Lesson 468. Simulating Async Tasks

--- UnitTesting/src/app/user/user.component.ts
+++ UnitTesting/src/app/user/user.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
+import { Data } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,19 +10,23 @@ import { UserService } from './user.service';
   imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
-  providers: [UserService]
+  providers: [UserService, DataService]
 })
 export class UserComponent implements OnInit {
 
   user: {name: string} = {name: ''}
   isLoggedIn: boolean = false
+  data: string
 
-  constructor(private userService: UserService){
+  constructor(
+    private userService: UserService, 
+    private dataService: DataService){
 
   }
 
   ngOnInit(): void {
       this.user = this.userService.user
+      this.dataService.getDetails().then((data: string) => this.data = data)
   }
 
 }
